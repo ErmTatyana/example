7cb36021f4f2c354c1939bdbd840eed08b93b722
Ermakova Tatyana <t.ermkova89@gmail.com>
2017-01-25 17:02:21 +0300

    ver2 simple calculator

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -7,9 +7,8 @@ int main()
 
     double arg_1, arg_2, rezult;
     char yourChoose;
+
     inputData(arg_1, arg_2, yourChoose);
-
-
     std::cout << "\n rezult = " << calculate(arg_1, arg_2, yourChoose);
 
     return 0;
